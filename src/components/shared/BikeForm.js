818c6fbd63f99736f30523407418aa57fe5a70c4
katherine-kania <katherine.kania@gmail.com>
2022-03-31 21:24:44 -0400

    Edit module and bikeForm created

---
+++ src/components/shared/BikeForm.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import { Form, Container, Button } from 'react-bootstrap'
+
+const BikeForm = ({ bike, handleSubmit, handleChange }) => (
+    <Container className="justify-content-center">
+        <Form onSubmit={handleSubmit}>
+            <Form.Label>City</Form.Label>
+            <Form.Control 
+                placeholder="what is the city name?"
+                value={bike.city}
+                name='city'
+                onChange={handleChange}
+            />
+            <Form.Label>State</Form.Label>
+            <Form.Control 
+                placeholder="If it's in the US, what state?"
+                value={bike.state}
+                name='state'
+                onChange={handleChange}
+            />
+            <Form.Label>Country</Form.Label>
+            <Form.Control 
+                placeholder="what is the country?"
+                value={bike.country}
+                name='country'
+                onChange={handleChange}
+            />
+            <Form.Control 
+                placeholder="How many stations are there?"
+                value={bike.numStations}
+                type='Number'
+                name='numStations'
+                onChange={handleChange}
+            />
+            <Form.Check 
+                label='Does the location have ebikes?'
+                name='ebikes'
+                defaultChecked={bike.ebikes}
+                onChange={handleChange}
+            />
+            <Button type='submit'>Submit</Button>
+        </Form>
+    </Container>
+)
+
+export default BikeForm
